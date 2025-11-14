e2a7d4af70081da710e5b8b70419c49347eb4a3e
Cammotron <cameron.astrong@gmail.com>
2025-11-14 00:32:04 -0600

    testing out rate song screen

--- src/main/java/edu/utsa/cs3443/projectdemo/HelloApplication.java
+++ src/main/java/edu/utsa/cs3443/projectdemo/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RowdyRhythemes.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 440);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("rate-song.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
